efb3c6ebd51db79701e1676a5d47caa4dafdbd36
Dulio Denis <dulio.denis@yahoo.com>
2021-06-13 16:54:12 -0400

    Added IBOutlet/IBAction and UITextFieldDelegate for the Search UITextField

--- clima-swift/Clima/Controller/WeatherViewController.swift
+++ clima-swift/Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,40 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchTextField.delegate = self
     }
 
-
+    @IBAction func searchTapped(_ sender: UIButton) {
+        print("Search text: \(searchTextField.text!)")
+        searchTextField.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("Search text: \(searchTextField.text!)")
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type a location"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searchTextField.text = ""
+    }
 }
 
