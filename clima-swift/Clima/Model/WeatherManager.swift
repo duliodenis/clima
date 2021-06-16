ccee9de050da89abb0915b296a27b2a4df318a97
Dulio Denis <dulio.denis@yahoo.com>
2021-06-15 21:20:49 -0400

    Add URLSession for networking in the WeatherManager

--- clima-swift/Clima/Model/WeatherManager.swift
+++ clima-swift/Clima/Model/WeatherManager.swift
@@ -15,6 +15,26 @@ struct WeatherManager {
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
-        print(urlString)
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            task.resume()
+        }
+        
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
     }
 }
