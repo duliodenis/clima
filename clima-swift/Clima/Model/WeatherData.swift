f12dc93d86d2fa55ade1cec3a198f95fe252b78e
Dulio Denis <dulio.denis@yahoo.com>
2021-06-16 23:14:03 -0400

    Create WeatherData structs that conform to Decodable for JSON decoding

---
+++ clima-swift/Clima/Model/WeatherData.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Dulio Denis on 6/16/21.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Decodable {
+    let temp: Float
+}
+
+struct Weather: Decodable {
+    let description: String
+}
--- clima-swift/Clima/Model/WeatherManager.swift
+++ clima-swift/Clima/Model/WeatherManager.swift
@@ -21,20 +21,27 @@ struct WeatherManager {
     func performRequest(urlString: String) {
         if let url = URL(string: urlString) {
             let session = URLSession(configuration: .default)
-            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+                    parseJSON(weatherData: safeData)
+                }
+            }
             task.resume()
         }
-        
     }
     
-    func handle(data: Data?, response: URLResponse?, error: Error?) {
-        if error != nil {
-            print(error!)
-            return
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print("Decoded data: \(decodedData.name) \(decodedData.main.temp) \(decodedData.weather[0].description)")
+        } catch {
+            print(error)
         }
-        if let safeData = data {
-            let dataString = String(data: safeData, encoding: .utf8)
-            print(dataString)
-        }
+        
     }
 }
