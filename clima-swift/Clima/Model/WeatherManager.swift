0bf9a4d8fd8cfc2533bcea4a9c0e4eb39425906f
Dulio Denis <dulio.denis@yahoo.com>
2021-06-17 22:58:40 -0400

    Add Weather Condition code in the WeatherManager

--- clima-swift/Clima/Model/WeatherManager.swift
+++ clima-swift/Clima/Model/WeatherManager.swift
@@ -39,9 +39,31 @@ struct WeatherManager {
         do {
             let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
             print("Decoded data: \(decodedData.name) \(decodedData.main.temp) \(decodedData.weather[0].description)")
+            print("id: \(decodedData.weather[0].id)")
+            print(getCondition(conditionId: decodedData.weather[0].id))
         } catch {
             print(error)
         }
-        
+    }
+    
+    func getCondition(conditionId: Int) -> String {
+        switch conditionId {
+                case 200...232:
+                    return "cloud.bolt"
+                case 300...321:
+                    return "cloud.drizzle"
+                case 500...531:
+                    return "cloud.rain"
+                case 600...622:
+                    return "cloud.snow"
+                case 701...781:
+                    return "cloud.fog"
+                case 800:
+                    return "sun.max"
+                case 801...804:
+                    return "cloud.bolt"
+                default:
+                    return "cloud"
+        }
     }
 }
